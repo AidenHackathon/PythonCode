a0ffe2706835ed895c61dfa7efcb65b6a627b94c
AidenHackathon <aiden.qin@student.tdsb.on.ca>
2021-06-26 13:04:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas as pd
 
-dataCSV = pd.read_csv("/Users/sula/Documents/GitHub/PythonCode/Testing.csv")
-dataList = dataCSV.iloc[:, 'How are you feeling?']
-print(dataList)
+dataCSV = pd.read_csv("/Users/sula/Documents/GitHub/PythonCode/Testing.csv", index_col="Timestamp")
+dataPerson1 = dataCSV.loc["Person 1"]
+dataList = dataPerson1.to_list()
 
-with open("/Users/sula/Documents/GitHub/PythonCode/reply.txt", mode='r') as f:
-    fileTxt = f.readlines()
-    highSchool = fileTxt[0]
+if(dataList[0] == "Frustrated"):
+    print("Fru")
 
-dice = {"1" : highSchool}
 
-str = dice.get("1")
-print(str)
+#with open("/Users/sula/Documents/GitHub/PythonCode/reply.txt", mode='r') as f:
+#    fileTxt = f.readlines()
+#    highSchool = fileTxt[0]
+
+#dice = {"1" : highSchool}
+
+#str = dice.get("1")
+#print(str)
