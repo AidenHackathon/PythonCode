80d28b376ee9c9bd812be2007dd560e4e971137b
AidenHackathon <aiden.qin@student.tdsb.on.ca>
2021-06-26 20:47:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-import pandas as pd
-from csv import reader
+with open("Test.csv", 'r') as file:
+  csv_reader = reader(file)
+  if(person == 0):
+    person += 1
+    continue
 
-with open("/Users/sula/Documents/GitHub/PythonCode/Testing.csv", mode='r') as readObject:
-    csv_Reader = reader(readObject)
-
-    for row in csv_Reader:
-        print(row[0])
-
-
-
-
-
-
-
-#with open("/Users/sula/Documents/GitHub/PythonCode/reply.txt", mode='r') as f:
-#    fileTxt = f.readlines()
-#    highSchool = fileTxt[0]
-
-#dice = {"1" : highSchool}
-
-#str = dice.get("1")
-#print(str)
